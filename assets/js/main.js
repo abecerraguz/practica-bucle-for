bb57a8de788b9cadbec172a3eb26de1ed9e90d9c
abecerraguz <alejandro.becerra@publimetro.cl>
2021-06-07 14:25:41 -0400

    Se guardan los cambios de practica bucles for

---
+++ assets/js/main.js
@@ -0,0 +1,75 @@
+for (var a = 0; a < 8; a++) {   
+    console.log(a);
+    /* Sale en la consola:
+    0
+    1
+    2
+    3
+    4
+    5
+    6
+    7
+    */
+}
+
+for (var b = 10; b < 100; b += 10) {   
+    console.log(b);
+    /* Sale en la consola:
+    10
+    20
+    30
+    40
+    50
+    50
+    70
+    80
+    90
+    */
+}
+
+for (var c = 16; c > 0; c--) {
+     console.log(c);
+    /* Sale en la consola:
+    16
+    15
+    14
+    13
+    12
+    11
+    10
+    9
+    8
+    7
+    6
+    5
+    4
+    3
+    2
+    1
+    */
+}
+
+for (var d = 0; d < 100; d++) {
+        console.log(d);
+        d += 5;
+    /* Sale en la consola:
+    0
+    6
+    12
+    24
+    30
+    36
+    42
+    48
+    54
+    60
+    66
+    72
+    78
+    84
+    90
+    96
+    */
+
+}
+    
